c386cd2bc3f8c8b14e5d4a94d21d3301debb26cb
Pradyum Tahekar <pradyum.tahekar@gmail.com>
2023-07-08 16:39:15 +0530

    Added the routers and completed create api succesfully

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { PORT } = require('./config/serverConfig');
-const CityRepository = require('./repository/city-repository');
+const ApiRoutes = require('./routes/index');
+// const CityRepository = require('./repository/city-repository');
 
 const setupAndStartServer = async () => {
 
@@ -12,9 +13,11 @@ const setupAndStartServer = async () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    app.use('/api',ApiRoutes);
+
     app.listen(PORT, async () => {
         console.log(`Server started at ${PORT}`);
-        const repo = new CityRepository();
+        // const repo = new CityRepository();
         // repo.createCity({name:"Indore"});
         // repo.deleteCity(49);
         
